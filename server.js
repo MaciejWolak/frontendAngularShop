2146be8ea9b27a462bfdb42e75091ad95fbf8466
MaciejWolak <maciejwolak@gmail.com>
2021-06-23 20:03:02 +0200

    changed server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webshopwolak'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/webshopwolak/'}),
-);
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
