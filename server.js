bc304a1446431ff4d5a7d416b29704a4c2091d1e
Maciej <58191262+MaciejWolak@users.noreply.github.com>
2021-06-27 09:46:44 +0200

    Update server.js
    5

--- server.js
+++ server.js
@@ -1,67 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-/* -------------------------------------------------------------------------- */
-
-app.get("/no-cors", (req, res) => {
-  console.info("GET /no-cors");
-  res.json({
-    text: "You should not see this via a CORS request."
-  });
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
-/* -------------------------------------------------------------------------- */
-
-app.head("/simple-cors", cors(), (req, res) => {
-  console.info("HEAD /simple-cors");
-  res.sendStatus(204);
-});
-app.get("/simple-cors", cors(), (req, res) => {
-  console.info("GET /simple-cors");
-  res.json({
-    text: "Simple CORS requests are working. [GET]"
-  });
-});
-app.post("/simple-cors", cors(), (req, res) => {
-  console.info("POST /simple-cors");
-  res.json({
-    text: "Simple CORS requests are working. [POST]"
-  });
-});
-
-/* -------------------------------------------------------------------------- */
-
-app.options("/complex-cors", cors());
-app.delete("/complex-cors", cors(), (req, res) => {
-  console.info("DELETE /complex-cors");
-  res.json({
-    text: "Complex CORS requests are working. [DELETE]"
-  });
-});
-
-/* -------------------------------------------------------------------------- */
-
-const issue2options = {
-  origin: true,
-  methods: ["POST"],
-  credentials: true,
-  maxAge: 3600
-};
-app.options("/issue-2", cors(issue2options));
-app.post("/issue-2", cors(issue2options), (req, res) => {
-  console.info("POST /issue-2");
-  res.json({
-    text: "Issue #2 is fixed."
-  });
-});
-
-/* -------------------------------------------------------------------------- */
-
-if (!module.parent) {
-  const port = process.env.PORT || 3001;
-
-  app.listen(port, () => {
-    console.log("Express server listening on port " + port + ".");
-  });
-}
